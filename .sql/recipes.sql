c316c2722eaf09edbb1e674171f660e76de9dfc8
Chad Lubrecht <chad@calubrecht.us>
2023-01-23 22:16:13 -0500

    Fix token timeout bug, Allow for emojis in text fields

--- .sql/recipes.sql
+++ .sql/recipes.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `recipes` (
   `userId` int NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `text` varchar(800) NOT NULL,
-  `keyIngredients` varchar(8000) NOT NULL DEFAULT '[]',
-  `commonIngredients` varchar(8000) NOT NULL DEFAULT '[]',
+  `name` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `text` varchar(800) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `keyIngredients` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '[]',
+  `commonIngredients` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '[]',
   `id` int NOT NULL,
   `sortOrder` int NOT NULL DEFAULT '2147483647',
   PRIMARY KEY (`userId`,`id`),
