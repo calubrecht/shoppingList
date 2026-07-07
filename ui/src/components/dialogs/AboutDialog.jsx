a5fad0712842ae8c196e5fdfb5dec9d5c2134ffe
Chad Lubrecht <chad@calubrecht.us>
2026-07-07 13:59:04 -0400

    Update about copyright

--- ui/src/components/dialogs/AboutDialog.jsx
+++ ui/src/components/dialogs/AboutDialog.jsx
@@ -5,7 +5,7 @@ export default function AboutDialog({ onClose }) {
         <span className="close" onClick={onClose}>&times;</span>
         <h1>Your Shopping List</h1>
         <p>A simple app for managing your shopping lists.</p>
-        <p>Copyright: Chad Lubrecht 2020</p>
+        <p>Copyright: Chad Lubrecht 2026</p>
       </div>
     </div>
   )
